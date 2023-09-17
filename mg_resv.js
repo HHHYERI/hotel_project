608c3153e87e8556c812b62fcb16f146d7997b4f
HHHYERI <145267270+HHHYERI@users.noreply.github.com>
2023-09-17 23:15:09 +0900

    Add files via upload

---
+++ mg_resv.js
@@ -0,0 +1,9 @@
+<script>
+$(function(){
+    $('#resv-search').click(function(){
+	    if($("#resv-list").css("display") == "none"){
+            $('#resv-list').show();
+    	}
+    })
+});
+</script>
